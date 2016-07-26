4be91e8aecad1a4b0c3790396a1621888665ca61
RxBx <rxbabcock@yahoo.com>
2016-07-25 17:05:37 -0700

    add Google Map elements to table thru API library

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
 
@@ -11,11 +12,15 @@ class WeatherList extends Component {
 		const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => 1.8*(temp - 273) + 32);
 		const pressures = cityData.list.map(weather => weather.main.pressure);
 		const humidities = cityData.list.map(weather => weather.main.humidity);
-		//console.log(temps);
+		//const lat = cityData.city.coord.lat);
+		//const lon = cityData.city.coord.lon);
+		//ES6 destructure method for multiple assignation
+		const {lat, lon } = cityData.city.coord;
+		
 
 		return (
 			<tr key={name}>
-				<td>{name}</td>
+				<td><GoogleMap lat={lat} lon={lon} /></td>
 				<td><Chart data={temps} color="orange" units="F"/></td>
 				<td><Chart data={pressures} color="green" units="hPa"/></td>
 				<td><Chart data={humidities} color="black" units="%"/></td>		
